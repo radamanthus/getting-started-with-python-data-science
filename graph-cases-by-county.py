7d9b00867cb40340466351807ce4afe6d5e289d5
Radamanthus Batnag <radamanthus@gmail.com>
2020-06-29 02:49:55 +0800

    Use pandas in graph-cases-by-county.py

--- graph-cases-by-county.py
+++ graph-cases-by-county.py
@@ -2,15 +2,9 @@ import pandas as pd
 import matplotlib, matplotlib.pyplot as plt
 
 data = pd.read_csv("us-counties.csv")
-dateIndex = data[0].index("date")
-casesIndex = data[0].index("cases")
-deathsIndex = data[0].index("deaths")
-fipsIndex = data[0].index("fips")
 
 county = input("County FIPS: ")
-rowsForCounty = [row for row in data if row[fipsIndex] == county]
+county_data = data.query(f'fips == "{county}"').drop(['county', 'state', 'fips'], axis = 1)
 
-for row in rowsForCounty:
-    print("Date: " + row[dateIndex] + ", Cases: " + row[casesIndex] + ", Deaths: " + row[deathsIndex])
-
+print(county_data)
 
